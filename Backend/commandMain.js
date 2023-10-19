c47949252e2d14494de27110924d486fe22ab2a7
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2023-10-19 19:48:47 -0400

    Added Prototype

---
+++ Backend/commandMain.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+// may not be necessary
+function loadCommands(path) {
+	if (!path) {
+		path = "./commands/";
+	}
+
+	files = fs.readdirSync(path);
+	console.log(files);
+	return files;
+}
+
+function processCommand(cmd) {
+	import(`./commands/${cmd}.js`).then((mod) => {
+		var output = mod.runSelf();
+		return output;
+	}).catch((err) => {
+		console.log(err.message + " this command does not exist.");
+	});
+
+	return output;
+}
+
+async function command(cmd) {
+	// searches for a file in the "commands" directory and then runs the "main" method in that file.
+	try {
+		let mod = await import(`./commands/${cmd}.js`);
+		let output = mod.runSelf();
+		console.log(output);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+command("whoami");
+
+//console.log(text);
---
+++ Backend/commands/whoami.js
@@ -0,0 +1,6 @@
+function runSelf() {
+	console.log("whoami is working");
+	return "This is an example output\n\nHeyo my name is Scott ayo pawo";
+}
+
+module.exports.runSelf = runSelf;
