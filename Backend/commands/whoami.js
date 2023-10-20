074201053d4169d2f11b28a30733058df4f1645b
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2023-10-19 20:13:09 -0400

    Updated whoami script

--- Backend/commands/whoami.js
+++ Backend/commands/whoami.js
@@ -1,6 +1,24 @@
 function runSelf() {
-	console.log("whoami is working");
-	return "This is an example output\n\nHeyo my name is Scott ayo pawo";
+	let output = ```
+		<section class="previousCommand">
+            <span class="user"><span class="green">guest@scott1e.com</span>:<span class="steel">~</span>$</span>
+            <span id="whoami">whoami</span>
+            <p>
+                Hey! My name is Scott, and I'm super cool and stuff :D I like blogging I hope,
+                because that's what this site is all about!<br><br>
+
+                :D<br><br>
+
+                <pre> <!-- https://www.asciiart.eu/text-to-ascii-art -->
+                    ░█▀▀░█▀▀░█▀█░▀█▀░▀█▀░▀█░░█▀▀
+                    ░▀▀█░█░░░█░█░░█░░░█░░░█░░█▀▀
+                    ░▀▀▀░▀▀▀░▀▀▀░░▀░░░▀░░▀▀▀░▀▀▀
+                    
+                    
+                </pre>
+            </p>
+        </section>```;
+	return output;
 }
 
 module.exports.runSelf = runSelf;
