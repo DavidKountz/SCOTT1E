57ad9a78a64c42eb48f88131695b7f86be5188fc
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2023-10-30 21:10:35 -0400

    Update console.js

--- controllers/console.js
+++ controllers/console.js
@@ -1,4 +1,8 @@
-import command from "../models/commandMain.js";
+// import command from "../models/commandMain.js";
+// I cannot do this ^ this is trying to blend client-side with server-side JS and is impossible
+// TODO: send a POST request with the command I want to send to the server
+// TODO: create a receiver for that request, and act on that information
+
 let history = document.getElementById("history");
 let cli = document.getElementById("cliInterface");
 
