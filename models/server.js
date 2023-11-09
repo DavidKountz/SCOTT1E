9718fd70cc345daf542c6f5bb18ad15f0d1360f5
wiz-rd <126345400+wiz-rd@users.noreply.github.com>
2023-11-09 14:42:46 -0500

    Testing URLs

--- models/server.js
+++ models/server.js
@@ -1,17 +1,44 @@
 // handles POST requests from the clients. Should be things such as changing dirs,
 // running commands, and opening files
 
-require('commandMain.js')();
+// const command = require('commandMain.js');
+const http = require("http");
+const fs = require("fs");
+const commands = []
+const template = `
+<h1>This is an article example</h1>
+<p id="data-to-input"></p>
+`;
 
-function receivePost() {
-    let command = true;
 
-    if (command) {
-        runCommand();
+
+const server = http.createServer((req, res) => {
+    if (req.url.includes("articles")) {
+        res.write();
+        res.end();
+    } else {
+        res.write("article!!!!!!!!!!!!!");
+        res.end();
     }
-}
 
-function runCommand(cmd, username, dir) {
-    command(cmd, username, dir);
-}
+});
+server.on('connection', (socket) => {
+    console.log("new connection");
+});
+server.listen(3000);
+console.log("Listening on port 3000");
 
+
+//
+// function receivePost() {
+//     let command = true;
+//
+//     if (command) {
+//         runCommand();
+//     }
+// }
+//
+// function runCommand(cmd, username, dir) {
+//     command.command(cmd, username, dir);
+// }
+//
