b96c41393a76cf1cd63ff4ed43b7f6a344758b8a
thegns22 <131874177+thegns22@users.noreply.github.com>
2023-11-30 16:01:47 -0500

    pop

--- Model/user.js
+++
@@ -1,7 +0,0 @@
-const user = {
-
-    userName: "author1",
-    password: "author1"
-
-}
-
---
+++ controllers/AuthorLog.js
@@ -0,0 +1,23 @@
+
+const loginForm = document.getElementById('loginForm');
+
+
+
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const usernameInput = document.getElementById('username').value;
+    const passwordInput = document.getElementById('password').value;
+
+    if (usernameInput === user.userName && passwordInput === user.password) {
+
+        console.log('Before redirection');
+        window.location.href = '/profile.html'; // Use an absolute URL
+        console.log('After redirection');
+
+
+    } else {
+        alert('Invalid username or password. Please try again.');
+    }
+});
+
+
