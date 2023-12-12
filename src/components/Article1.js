b97fdac1a0ab860cb4af160c8195087dfb6ba878
AzlaanJuma <n01035592@unf.edu>
2023-12-12 11:54:20 -0500

    commit

--- src/components/Article1.js
+++ src/components/Article1.js
@@ -1,6 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import Text from "./textfiles/article1.txt";
-import "./Profile.css"
+import "./Profile.css";
+
+const DisqusScript = () => {
+    useEffect(() => {
+        var disqus_config = function () {
+            this.page.url = window.location.href;
+            this.page.identifier = "Page" + window.location.href;
+        };
+
+        (function () {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://EXAMPLE.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    }, []);
+
+    return null; // Since this is a script, it doesn't render anything
+};
 
 const Article1 = () => {
     const [fileContent, setFileContent] = useState("");
@@ -22,20 +40,21 @@ const Article1 = () => {
         fetchData();
     }, []);
 
-
     return (
-        <html lang="en">
-        <head>
-            <title>Article Page</title>
-        </head>
-        <body>
-        <h1>My Article</h1>
-        <p>{fileContent}</p>
-        </body>
-        </html>
+        <>
+            <style>
+                {`
+                    iframe[src*="ads-iframe"] { display: none; }
+                `}
+            </style>
+            <div>
+                <h1>My Article</h1>
+                <p>{fileContent}</p>
+                <div id="disqus_thread"></div>
+                <DisqusScript />
+            </div>
+        </>
     );
 };
 
-
 export default Article1;
-
